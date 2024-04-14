5dd712abbbb67d7f4cdd343685e36310a21e8a6e
dtcpassdelhigovin <129848142+dtcpassdelhigovin@users.noreply.github.com>
2024-04-14 14:56:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="YOUR URL";
+var hostURL="https://track-down-aarnx3fuf-dtcpassdelhigovin1s-projects.vercel.app/";
 //TOGGLE for Shorters
 var use1pt=false;
 
